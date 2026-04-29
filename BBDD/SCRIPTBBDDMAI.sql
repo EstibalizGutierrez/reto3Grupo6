5abf2c43aaad0f914b8ee9711a8e8643e521abe5
Maitane Azueta <maitane.azuetahe@elorrieta-errekamari.com>
2026-04-29 13:34:06 +0200

    relaciones hechas, por revisar

--- BBDD/SCRIPTBBDDMAI.sql
+++ BBDD/SCRIPTBBDDMAI.sql
@@ -1,9 +1,14 @@
 CREATE DATABASE spotify COLLATE utf8mb4_spanish_ci;
 USE spotify;
 
+/* --------------TABLA DE IDIOMA-------------- */
+DROP TABLE Idioma;
+CREATE TABLE Idioma (
+	IdIdioma ENUM('ES', 'EU', 'EN', 'FR', 'DE', 'CA', 'GA', 'AR') PRIMARY KEY,
+    Descripcion TEXT NOT NULL
+);
+
 /* --------------TABLA DE ARTISTA-------------- */
-/* relacion hecha artista-podcaster, artista-musico
-FALTA CON ALBUM */
 DROP TABLE Artista;
 CREATE TABLE Artista (
 	IdArtista CHAR(5) PRIMARY KEY,
@@ -14,68 +19,38 @@ CREATE TABLE Artista (
 );
 
 /* --------------TABLA DE PODCASTER-------------- */
-/* relacion hecha artista-podcaster 
-FALTA PODCAST*/
 DROP TABLE Podcaster;
 CREATE TABLE Podcaster (
 	IdPodcaster CHAR(5) PRIMARY KEY,
-    IdArtista CHAR(5) NOT NULL,
-    UNIQUE (IdArtista),
-    CONSTRAINT fk_artista_podcaster
-		FOREIGN KEY (IdArtista) REFERENCES Artista(IdArtista) 
-        ON DELETE CASCADE
+    IdArtista CHAR(5) NOT NULL UNIQUE,
+    CONSTRAINT fk_artista_podcaster FOREIGN KEY (IdArtista) 
+		REFERENCES Artista(IdArtista) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE MÚSICO-------------- */
-/* relacion hecha artista-musico 
-FALTA ALBUM*/
 DROP TABLE Músico;
 CREATE TABLE Músico (
 	IdMusico CHAR(5) PRIMARY KEY,
     Característica ENUM('Solista', 'Grupo') ,
-    IdArtista CHAR(5) NOT NULL,
-    CONSTRAINT fk_artista_musico
-		FOREIGN KEY (IdArtista) REFERENCES Artista(IdArtista) 
-        ON DELETE CASCADE
+    IdArtista CHAR(5) NOT NULL UNIQUE,
+    CONSTRAINT fk_artista_musico FOREIGN KEY (IdArtista) 
+		REFERENCES Artista(IdArtista) ON DELETE CASCADE
 );
 
-/* --------------TABLA DE CLIENTE-------------- */
-/* 
-FALTA PLAYLIST, PREMIUM Y GUSTOS*/
-DROP TABLE Cliente;
-CREATE TABLE Cliente (
-	IdCliente CHAR(5) PRIMARY KEY,
-    Nombre VARCHAR(25) NOT NULL,
-    Apellido VARCHAR(25) NOT NULL,
-    Característica ENUM('ES', 'EU', 'EN', 'FR', 'DE', 'CA', 'GA', 'AR'),
-    Usuario VARCHAR(25) NOT NULL UNIQUE,
-    Contraseña VARCHAR(30) NOT NULL,
-    FechaNacimiento DATE NOT NULL,
-    FechaRegistro DATE NOT NULL,
-    Tipo ENUM('Free', 'Premium')
-);
-
-/* --------------TABLA DE PREMIUM-------------- */
-/* 
-FALTA CLIENTE*/
-DROP TABLE Premium;
-CREATE TABLE Premium (
-	/* IdCliente CHAR(5) PRIMARY KEY, */
-	FechaCaducidad DATE NOT NULL
-);
-
-/* --------------TABLA DE IDIOMA-------------- */
-/* 
-FALTA CLIENTE Y PLAYLIST*/
-DROP TABLE Idioma;
-CREATE TABLE Idioma (
-	IdIdioma ENUM('ES', 'EU', 'EN', 'FR', 'DE', 'CA', 'GA', 'AR') PRIMARY KEY,
-    Descripcion TEXT NOT NULL
+/* --------------TABLA DE ALBUM-------------- */
+DROP TABLE Album;
+CREATE TABLE Album (
+	IdAlbum CHAR(5) PRIMARY KEY,
+    Título VARCHAR(50) NOT NULL,
+    Año DATE NOT NULL,
+    Genero VARCHAR(25) NOT NULL,
+    Imagen LONGBLOB,
+    IdMusico CHAR(5) NOT NULL UNIQUE,
+    CONSTRAINT fk_musico_album FOREIGN KEY (IdMusico) 
+        REFERENCES Músico(IdMusico) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE AUDIO-------------- */
-/* 
-FALTA PODCAST, GUSTOS Y CANCION*/
 DROP TABLE Audio;
 CREATE TABLE Audio (
 	IdAudio CHAR(5) PRIMARY KEY, 
@@ -87,65 +62,85 @@ CREATE TABLE Audio (
 );
 
 /* --------------TABLA DE PODCAST-------------- */
-/* 
-FALTA PODCASTER Y AUDIO*/
 DROP TABLE Podcast;
 CREATE TABLE Podcast (
-	/* IdAudio CHAR(5) PRIMARY KEY, */
-    NºColaboradores INT
-    /* IdPodcaster CHAR(5) NOT NULL */
+	IdAudio CHAR(5) PRIMARY KEY,
+    NºColaboradores INT,
+    IdPodcaster CHAR(5) NOT NULL,
+    CONSTRAINT fk_audio_podcast FOREIGN KEY (IdAudio) 
+        REFERENCES Audio(IdAudio) ON DELETE CASCADE,
+    CONSTRAINT fk_podcaster_podcast FOREIGN KEY (IdPodcaster) 
+        REFERENCES Podcaster(IdPodcaster) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE CANCION-------------- */
-/* 
-FALTA PLAYLIST_CANCIONES, AUDIO Y ALBUM*/
 DROP TABLE Canción;
 CREATE TABLE Canción (
 	IdCancion CHAR(5) PRIMARY KEY,
-    /* IdAlbum CHAR(5) NOT NULL */
-    ArtistasInvitados TEXT
+    IdAlbum CHAR(5) NOT NULL,
+    ArtistasInvitados TEXT,
+    CONSTRAINT fk_audio_cancion FOREIGN KEY (IdAudio) 
+        REFERENCES Audio(IdAudio) ON DELETE CASCADE,
+    CONSTRAINT fk_album_cancion FOREIGN KEY (IdAlbum) 
+        REFERENCES Album(IdAlbum) ON DELETE CASCADE
 );
 
-/* --------------TABLA DE ALBUM-------------- */
-/* 
-FALTA CANCION Y MUSICO*/
-DROP TABLE Album;
-CREATE TABLE Album (
-	IdAlbum CHAR(5) PRIMARY KEY,
-    Título VARCHAR(25) NOT NULL,
-    Año DATE NOT NULL,
-    Genero VARCHAR(25) NOT NULL,
-    Imagen LONGBLOB
-    /*IdMusico CHAR(5) NOT NULL */
+/* --------------TABLA DE CLIENTE-------------- */
+DROP TABLE Cliente;
+CREATE TABLE Cliente (
+	IdCliente CHAR(5) PRIMARY KEY,
+    Nombre VARCHAR(25) NOT NULL,
+    Apellido VARCHAR(25) NOT NULL,
+    Característica ENUM('ES', 'EU', 'EN', 'FR', 'DE', 'CA', 'GA', 'AR'),
+    Usuario VARCHAR(25) NOT NULL UNIQUE,
+    Contraseña VARCHAR(30) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    FechaRegistro DATE NOT NULL,
+    Tipo ENUM('Free', 'Premium'),
+	CONSTRAINT fk_idioma_cliente FOREIGN KEY (IdIdioma) 
+		REFERENCES Idioma(IdIdioma) ON DELETE CASCADE
+);
+
+/* --------------TABLA DE PREMIUM-------------- */
+DROP TABLE Premium;
+CREATE TABLE Premium (
+	IdCliente CHAR(5) PRIMARY KEY,
+	FechaCaducidad DATE NOT NULL,
+	CONSTRAINT fk_cliente_premium FOREIGN KEY (IdCliente) 
+		REFERENCES Cliente(IdCliente) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE PLAYLIST-------------- */
-/* 
-FALTA PLAYLISTCANCIONES Y CLIENTE*/
 DROP TABLE Playlist;
 CREATE TABLE Playlist (
-	IdList INT(5) PRIMARY KEY,
-    Título VARCHAR(25) NOT NULL,
-    FechaCreacion DATE NOT NULL
-    /* IdCliente CHAR(5) NOT NULL */
+	IdList INT(5) PRIMARY KEY AUTO_INCREMENT,
+    Título VARCHAR(50) NOT NULL,
+    FechaCreacion DATE NOT NULL,
+    IdCliente CHAR(5) NOT NULL,
+    CONSTRAINT fk_cliente_playlist FOREIGN KEY (IdCliente) 
+        REFERENCES Cliente(IdCliente) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE PLAYLIST-CANCIONES-------------- */
-/* 
-FALTA CANCION Y PLAYLIST*/
 DROP TABLE Playlist_Canciones;
 CREATE TABLE Playlist_Canciones (
-    FechaPlayList_Cancion DATE NOT NULL
+    IdList INT(5) PRIMARY KEY,
+    IdAudio CHAR(5) PRIMARY KEY,
+    FechaPayList_cancion DATE NOT NULL,
+    CONSTRAINT fk_playlist_vinculo FOREIGN KEY (IdList) 
+        REFERENCES Playlist(IdList) ON DELETE CASCADE,
+    CONSTRAINT fk_cancion_vinculo FOREIGN KEY (IdAudio) 
+        REFERENCES Canción(IdAudio) ON DELETE CASCADE
 );
 
 /* --------------TABLA DE GUSTOS-------------- */
-/* 
-FALTA CLIENTE Y AUDIO*/
-/*
 DROP TABLE Gustos;
 CREATE TABLE Gustos (
-	IdCliente CHAR(5) NOT NULL
-    IdAudio CHAR(5) NOT NULL 
+	IdCliente CHAR(5) PRIMARY KEY,
+    IdAudio CHAR(5) PRIMARY KEY,
+    CONSTRAINT fk_gustos_cliente FOREIGN KEY (IdCliente) 
+        REFERENCES Cliente(IdCliente) ON DELETE CASCADE,
+    CONSTRAINT fk_gustos_audio FOREIGN KEY (IdAudio) 
+        REFERENCES Audio(IdAudio) ON DELETE CASCADE
 );
-*/
 
