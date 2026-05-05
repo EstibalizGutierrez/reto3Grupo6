3534e24a3e3b2cd7352a07716d525edcb7a1ad04
Estibaliz Gutierrez <estibalizzahiragutierrezgo@elorrieta-errekamari.com>
2026-05-05 21:03:17 +0200

    primera vista hecha y paginas de html creadas con base

--- BBDD/vistas.sql
+++ BBDD/vistas.sql
@@ -1,2 +1,38 @@
 /*vista medio hecha*/
-create view TopMejoresCanciones as select idcancion from cancion where count(nreproducciones);
+CREATE VIEW vista_ranking_contenido AS
+SELECT 
+    A.IdAudio,
+    A.Nombre AS Titulo,
+    A.Tipo,
+    A.NReproducciones,
+    CASE 
+        WHEN A.Tipo = 'Canción' THEN (SELECT Art.NombreArtistico 
+                                      FROM Artista Art 
+                                      JOIN Musico M ON Art.IdArtista = M.IdMusico
+                                      JOIN Album Alb ON M.IdMusico = Alb.IdMusico
+                                      JOIN Cancion C ON Alb.IdAlbum = C.IdAlbum
+                                      WHERE C.IdCancion = A.IdAudio)
+        WHEN A.Tipo = 'Podcast' THEN (SELECT Art.NombreArtistico 
+                                      FROM Artista Art 
+                                      JOIN Podcaster P ON Art.IdArtista = P.IdPodcaster
+                                      JOIN Podcast Pod ON P.IdPodcaster = Pod.IdPodcaster
+                                      WHERE Pod.IdPodcast = A.IdAudio)
+    END AS Autor
+FROM Audio A
+ORDER BY A.NReproducciones DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
