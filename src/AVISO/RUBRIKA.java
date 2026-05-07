8b9ae0a3931447c2249f1217a8ea5036d9b901ce
Jhon Izarra <jhonsebastian.izarrava@elorrieta-errekamari.com>
2026-05-07 21:18:43 +0200

    subida java (leer paquete AVISO)

---
+++ src/AVISO/RUBRIKA.java
@@ -0,0 +1,26 @@
+package AVISO;
+
+public class RUBRIKA {
+
+	
+	/*
+	 * RUBRICA (SE PUEDE CAMBIAR PERO CONSULTAR)
+	 * 
+	 * 1. EN EL PAQUEE DAOs se pone todos los daos de las consultas mysql
+	 * 2. EN EL PAQUETE POJOs se ponen todas las clases que realizaremos
+	 * 3. EN EL PAQUETE UTILS por ahora dejamos solo el metodo conexion de la base de datos (implementar en cada clase DAO)
+	 * 
+	 * NOMBRE DE LAS VARIABLES DE LAS VENTANAS
+	 * (EJEMPLOS)
+	 * label = lblUsuario, lblContrasena...
+	 * button = botonLogin, botonRegistro...
+	 * textfield = txtUsuario, txtContrasena...
+	 * radioButton = radioBotonEleccion...
+	 * textArea = txtAreaCanciones...
+	 * table = tablaCanciones...
+	 * 
+	 * */
+	
+	
+	
+}
---
+++ src/UTILS/Conexion.java
@@ -0,0 +1,58 @@
+package UTILS;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	private final String URL = "jdbc:mysql://localhost:33060/reto3spotify";
+	private final String USER = "root";											//CAMBIARLO DEPENDIENDO DE LA RUTA , USER Y PASSWD DE TU ORDENADOR
+	private final String PASSWORD = "elorrieta";
+	private Connection conexion;
+	
+	public Connection getConnection() {
+		
+		try {
+			
+			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+			if (conexion != null) {
+				
+				System.out.println("Conexión exitosa");
+				
+			} 
+			
+		} catch (SQLException error) {
+			
+			System.out.println("Error de SQL: " + error.getMessage());
+			error.printStackTrace();
+			
+		} catch (Exception error2) {
+			
+			System.out.println("Error inesperado: " + error2.getMessage());
+			error2.printStackTrace();
+			
+		}
+		
+		return conexion;
+		
+	}
+	
+	public void cerrarConexion() {
+		
+		try {
+			
+			conexion.close();
+			
+		} catch (SQLException error) {
+			
+			System.out.println("No se ha podido cerrar la conexión");
+			error.printStackTrace();
+			
+		}
+		
+	}
+	
+	
+}
