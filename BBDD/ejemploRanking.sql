b27db462dd9b96d05174ce0119cb5d0256334412
Jhon Izarra <jhonsebastian.izarrava@elorrieta-errekamari.com>
2026-05-07 21:18:43 +0200

    subida java (leer paquete AVISO)

---
+++ BBDD/ejemploRanking.sql
@@ -0,0 +1,22 @@
+use reto3spotify;
+CREATE VIEW vista_ranking_contenido AS
+SELECT 
+    row_number() OVER (ORDER BY NReproducciones DESC) AS 'Ranking',
+    A.Nombre AS Titulo,
+    A.Tipo,
+    A.NReproducciones,
+    CASE 
+        WHEN A.Tipo = 'Canción' THEN (SELECT Art.NombreArtistico 
+                                      FROM Artista Art 
+                                      JOIN Musico M ON Art.IdArtista = M.IdMusico
+                                      JOIN Album Alb ON M.IdMusico = Alb.IdMusico
+                                      JOIN Cancion C ON Alb.IdAlbum = C.IdAlbum
+                                      WHERE C.IdCancion = A.IdAudio)
+        WHEN A.Tipo = 'Podcast' THEN (SELECT Art.NombreArtistico 
+                                      FROM Artista Art 
+                                      JOIN Podcaster P ON Art.IdArtista = P.IdPodcaster
+                                      JOIN Podcast Pod ON P.IdPodcaster = Pod.IdPodcaster
+                                      WHERE Pod.IdPodcast = A.IdAudio)
+    END AS Autor
+FROM Audio A
+ORDER BY A.NReproducciones DESC;
