abf2643f965845db10924399e1168cfcd70436a5
grupo4reto2 <jhonizarra26@gmail.com>
2026-05-11 18:06:36 +0200

    Update

---
+++ src/DAOs/ClienteDAO.java
@@ -0,0 +1,169 @@
+package DAOs;
+
+import javax.swing.*;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.time.LocalDate;
+import POJOs.Cliente;
+import POJOs.Enums.idIdioma;
+import POJOs.Enums.tipoUsuario;
+import UTILS.Conexion;
+
+public class ClienteDAO {
+		
+	private Conexion conn = new Conexion();
+	private PreparedStatement statement;
+	private ResultSet resultSet;
+	private String clienteInsertado = "¡¡Cliente creado correctamente!!";
+	private String sqlError = "No se ha podido registrar al cliente";
+	
+	
+
+	/**
+	 * Método para validar el login y obtener los datos del cliente
+	 * @param Cliente datos
+	 * @return Cliente
+	 */
+	public Cliente clienteLogin (Cliente datos) {
+			
+	Connection conexion = null;
+	Cliente clienteLogueado = null; 
+
+	String sql = "Select * from Clientes where Usuario = ? and Contrasena = ?";
+			
+		try {
+				
+			conexion = conn.getConnection();
+				
+			if (conexion != null) {
+					
+				statement = conexion.prepareStatement(sql);
+				statement.setString(1, datos.getUsuario());
+				statement.setString(2, datos.getContrasena());
+				resultSet = statement.executeQuery();
+					
+				if (resultSet.next()) {
+						
+					// Si existe el cliente en el login lo rellenamos con sus datos de la BBDD
+					
+						clienteLogueado = new Cliente(
+						resultSet.getString("Usuario"),
+						resultSet.getString("Contrasena"),
+						resultSet.getString("Nombre"),
+						resultSet.getString("Apellido"),
+						idIdioma.valueOf(resultSet.getString("Idioma")),
+						resultSet.getDate("FechaNacimiento").toLocalDate(),
+						resultSet.getDate("FechaRegistro").toLocalDate(),
+						tipoUsuario.valueOf(resultSet.getString("Tipo"))
+	
+							);
+						
+						JOptionPane.showMessageDialog(null, "Login exitoso para el cliente" , clienteLogueado.getNombre(), 0);
+						
+						}
+					
+				}
+				
+			} catch (SQLException error) {
+				
+				JOptionPane.showMessageDialog(null, "Cliente no encontrado");
+				
+			} 
+			return clienteLogueado;
+			
+		}
+
+		/**
+		 * Método para registrar un nuevo cliente a la BBDD
+		 * @param Cliente nuevo
+		 * @return boolean
+		 */
+		public boolean clienteInsertar (Cliente nuevo) {
+			
+			Connection conexion = null;
+			boolean clienteValidoInsertado = false;
+			String idNuevo = generarId();
+
+			//CONSULTA SQL
+			String sql = "Insert into Clientes (Usuario, Contrasena, Nombre, Apellido, Idioma, FechaNacimiento, FechaRegistro, Tipo) "
+					+ 	"values (?, ?, ?, ?, ?, ?, ?, ?)";
+			
+			try {
+				
+				conexion = conn.getConnection();
+				
+				if (conexion != null) {
+					
+					statement.setString(1, idNuevo); 
+		            statement.setString(2, nuevo.getUsuario());
+		            statement.setString(3, nuevo.getContrasena());
+		            statement.setString(4, nuevo.getNombre());
+		            statement.setString(5, nuevo.getApellido());
+		            statement.setString(6, nuevo.getIdioma().name());
+		            statement.setDate(7, Date.valueOf(nuevo.getFechaNacimiento()));
+		            statement.setDate(8, Date.valueOf(LocalDate.now()));  
+		            statement.setString(9, nuevo.getTipo().name());
+					
+					int filas = statement.executeUpdate();//Lo hacemos para ver si se han incluido las filas en la BBDD
+					
+					if (filas > 0) { //con que se haya insertado al menos una fila ya sabremos que se han insertado las otras
+						
+						clienteValidoInsertado = true;
+						
+						JOptionPane.showMessageDialog(null, clienteInsertado);
+						
+					}
+					
+				}
+				
+			} catch (SQLException error) {
+				
+				JOptionPane.showMessageDialog(null, sqlError);
+				
+			} 
+			
+			return clienteValidoInsertado;
+			
+		}
+		
+		/**
+		 * metodo para generar el id del cliente de manera automatica
+		 * 
+		 * @return String
+		 */
+		 
+		public String generarId() {
+			Connection conexion = null;
+			String nuevoId = null;
+			String sql = "Select idCliente from clientes order by idCliente desc limit 1"; //SENTENCIa para guardar el id del ultimo cliente en un String
+			
+			try {
+				conexion = conn.getConnection();
+				
+				if (conexion != null) {
+					
+					statement = conexion.prepareStatement(sql);
+					resultSet = statement.executeQuery();
+					
+					if (resultSet.next()) {
+						String ultimoId = resultSet.getString("idCliente"); //guardamos el ultimo id en un string
+						
+						int num = Integer.parseInt(ultimoId.substring(2)); //Hacemos substring para OBTENER los ultimos 3 digitos y quitarnos la parte "CL" del id "CL004" (EJEMPLO)
+						
+						nuevoId = "CL" + String.format("%03d", num + 1); //sSumamos y lo formateamos con %03d para que solo acepte 3 numeros y que empiece por la izquierda con 0 asi como maximo solo podria existir CL999
+					}
+				}
+			} catch (SQLException error) {
+
+					error.printStackTrace();
+			}
+			
+			return nuevoId;
+		}
+
+	}
+	
+
--- src/aplicacion/Login.java
+++ src/aplicacion/Login.java
@@ -5,13 +5,34 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import POJOs.Cliente;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel panelUsuario;
+	private JTextField txtUsuario;
+	private JTextField txtContrasena;
+	private JLabel lblUsuario;
+	private JLabel lblContrasena;
+	private JComboBox comboBoxOpcionUsuario;
+	private JButton botonRegistro;
+	private Cliente clienteExistente;
 
 	/**
 	 * Launch the application.
@@ -34,11 +55,71 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1250, 800);
+		setBounds(100, 100, 950, 600);
+		setTitle("Login");
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Constantia", Font.PLAIN, 20));
+		lblUsuario.setBounds(301, 196, 116, 36);
+		contentPane.add(lblUsuario);
+		
+		lblContrasena = new JLabel("Contrasena:");
+		lblContrasena.setFont(new Font("Constantia", Font.PLAIN, 20));
+		lblContrasena.setBounds(301, 265, 116, 36);
+		contentPane.add(lblContrasena);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtUsuario.setBounds(417, 198, 143, 32);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		txtContrasena = new JTextField();
+		txtContrasena.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtContrasena.setColumns(10);
+		txtContrasena.setBounds(417, 265, 143, 32);
+		contentPane.add(txtContrasena);
+		
+		panelUsuario = new JPanel();
+		panelUsuario.setBackground(new Color(192, 192, 192));
+		panelUsuario.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(255, 0, 128), new Color(255, 255, 0), new Color(255, 255, 0), new Color(255, 0, 255)));
+		panelUsuario.setBounds(187, 75, 524, 364);
+		contentPane.add(panelUsuario);
+		panelUsuario.setLayout(null);
+		
+		String [] opciones = {"Cliente" , "Administrador"};
+		comboBoxOpcionUsuario = new JComboBox<>(opciones);
+		comboBoxOpcionUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		comboBoxOpcionUsuario.setBounds(248, 250, 124, 20);
+		panelUsuario.add(comboBoxOpcionUsuario);
+		
+		JButton botonLogin = new JButton("LOGIN");
+		botonLogin.setFont(new Font("Constantia", Font.BOLD, 15));
+		botonLogin.setBounds(121, 298, 118, 31);
+		botonLogin.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				String user = txtUsuario.getText();
+				String password = txtContrasena.getText();
+				
+				clienteExistente = new Cliente(user,password);
+				
+			}
+			
+		});
+		panelUsuario.add(botonLogin);
+		
+		botonRegistro = new JButton("REGISTRO");
+		botonRegistro.setFont(new Font("Constantia", Font.BOLD, 15));
+		botonRegistro.setBounds(265, 298, 118, 31);
+		panelUsuario.add(botonRegistro);
 
 	}
 }
