cfcaee7de21c9662f544343ef4ae6b8abb82049f
MaitaneAzueta <maitane.azuetahe@elorrieta-errekamari.com>
2026-05-18 19:28:44 +0200

    solucionado merge

--- BBDD/vistas.sql
+++ BBDD/vistas.sql
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-create view vista_ranking_contenido as
-select 
-    A.Nombre as Titulo,
-    A.Tipo,
-    A.NReproducciones,
-    case 
-        when A.Tipo = 'Canción' then (select Art.NombreArtistico 
-                                      from Artista Art 
-                                      join Musico M on Art.IdArtista = M.IdMusico
-                                      join Album Alb on M.IdMusico = Alb.IdMusico
-                                      join Cancion C on Alb.IdAlbum = C.IdAlbum
-                                      where C.IdCancion = A.IdAudio)
-        when A.Tipo = 'Podcast' then (select Art.NombreArtistico 
-                                      from Artista Art 
-                                      join Podcaster P on Art.IdArtista = P.IdPodcaster
-                                      join Podcast Pod on P.IdPodcaster = Pod.IdPodcaster
-                                      where Pod.IdPodcast = A.IdAudio)
-    end as Autor
-from Audio A
-order by A.NReproducciones desc;
-/*albumes mas escuchados*/
-/*artistas*/
-/*podcasters*/
-=======
 create view AlbumesMasEscuchados as
 select 
     al.IdAlbum,
@@ -61,7 +36,6 @@ join Podcast po on p.IdPodcaster = po.IdPodcaster
 join Audio a on po.IdPodcast = a.IdAudio
 group by ar.IdArtista, ar.NombreArtistico
 order by TotalReproducciones desc;
->>>>>>> 80e118177dbdb15824a6f69003ddf2c493e1fdb7
 
 
 
